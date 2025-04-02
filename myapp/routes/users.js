caffd167f3311593bef96c96f523317010002553
pilaruncal <pilarmartinezuncal@gmail.com>
2025-04-02 19:59:20 -0300

    requerimos el controllers de users

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const usersController= require("../controllers/usersController");
 
 router.get('/login', usersController.login);
 router.get("/register", usersController.register);
