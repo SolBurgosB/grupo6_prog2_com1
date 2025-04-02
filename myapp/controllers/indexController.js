f8d2977e501ebff13316c11340e785a5753c6968
SolBurgosB <sburgosbernasconi@udesa.edu.ar>
2025-04-02 19:50:11 -0300

    Creamos la ruta de search-results

--- myapp/controllers/indexController.js
+++ myapp/controllers/indexController.js
@@ -1,6 +1,11 @@
+const { search } = require("../routes");
+
 const indexController= {
-    index: function(req, res) {
+  index: function(req, res) {
     res.render('index');
-  }
+  },
+  search: function(req, res) {
+    res.render('search-results', req.params.producto); //completar
+  },
 } //para que se renderice (que aparezca) la vista index 
 module.exports=indexController;
