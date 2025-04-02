0650db5f0da4e1a070dc1e1254a10643caa5f878
pilaruncal <pilarmartinezuncal@gmail.com>
2025-04-02 19:50:18 -0300

    creamos las rutas y los controladores de login y register

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,10 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/login', usersController.login);
+router.get("/register", usersController.register);
 
 module.exports = router;
 
