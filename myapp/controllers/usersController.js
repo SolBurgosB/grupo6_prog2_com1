3e7328b4492c65d0fc2af32ff21638dff2bba754
Celinamaccari <cmaccari@udesa.edu.ar>
2025-04-11 15:47:08 -0300

    Creamos la ruta de profile y la reemplazamos en las vistas

--- myapp/controllers/usersController.js
+++ myapp/controllers/usersController.js
@@ -5,6 +5,9 @@ const usersController={
     register: function(req, res) {
         res.render('register');
     },
+    profile: function(req, res) {
+        res.render('profile');
+    },
 }
 
 module.exports = usersController;
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -4,6 +4,7 @@ const usersController= require("../controllers/usersController");
 
 router.get('/login', usersController.login);
 router.get("/register", usersController.register);
+router.get("/profile", usersController.profile);
 
 module.exports = router;
 
