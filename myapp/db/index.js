c1d002564a9c7d01cd0c513e39941603338ccf72
SolBurgosB <sburgosbernasconi@udesa.edu.ar>
2025-04-11 17:26:40 -0300

    Hicimos el CSS de login y register. Creamos el usuario, cambiamos el link de las imagenes y empezamos con el formulario de search-results

--- myapp/db/index.js
+++ myapp/db/index.js
@@ -1,71 +1,71 @@
 const index = { 
         usuario: {
-            mail: "",
-            username: "",
-            userpassword: "",
-            birthday: "",
-            dni: "",
-            profileimage: "",
+            id: 1,
+            mail: "tinistoessel@gmail.com",
+            username: "tinita99",
+            userpassword: "stoessel123",
+            birthday: "21/03/1999",
+            dni: "12345678",
+            profileimage: "/images/tini.jpg",
         },
         products: {
             lista: [
             {
                 id: 1,
-                productimage: "https://http2.mlstatic.com/D_NQ_NP_702169-MLA79667134087_092024-O.webp",
+                productimage: "/image/base.jpg",
                 productname: "Base líquida de larga duración",
                 productdescription: "Cobertura media a completa con acabado natural. Resiste todo el día sin sentirse pesada. Ideal para pieles mixtas a grasas.",
             },
             {
                 id: 2,
-                productimage: "https://www.maybelline.com.ar/-/media/project/loreal/brand-sites/mny/americas/latam/products/face/concealer/instant-age-rewind-concealer/product-packshot.jpeg?rev=a3f024830ea249daaff24fdc9d832df6&cx=0.25&cy=0.31&cw=315&ch=472&hash=2CBAAF902D73D62619168645E654C814",
-                productname: "Corrector iluminador",
+                productimage: "/image/corrector.jpg",
                 productdescription: "Cubre ojeras y manchas al instante con un efecto luminoso. Textura cremosa que se difumina fácil y no se acumula.",
             },
             {
                 id: 3,
-                productimage: "https://m.media-amazon.com/images/I/511naEb-UOL._AC_UF1000,1000_QL80_.jpg",
+                productimage: "/image/rubor.jpg",
                 productname: "Rubor en crema",
                 productdescription: "Aporta un toque de color suave y radiante que se integra perfectamente con la piel. Su fórmula liviana deja un acabado natural, ideal para realzar tu brillo.",
             },
             {
                 id: 4,
-                productimage: "https://farmaciasdelpueblo.vtexassets.com/arquivos/ids/178206/Revlon-Polvo-Traslucido-Colorstay-Blot-0309970165123_img1.png?v=637892742963630000",
+                productimage: "/image/polvo.jpg",
                 productname: "Polvo matificante traslúcido",
                 productdescription: "Sella el maquillaje y controla el brillo sin alterar el color. Acabado suave y sin flashback.",
             },
             {
                 id: 5,
-                productimage: "https://juleriaque.vtexassets.com/arquivos/ids/205595/hypnose-palette-5-couleurs-9808663CBA7CFDD04FAD9911C081B2F6.jpg?v=638085769004670000",
+                productimage: "/image/sombra.jpg",
                 productname: "Sombra de ojos en barra",
                 productdescription: " Fórmula cremosa de alta pigmentación que se aplica con facilidad. Deja un acabado natural y duradero, ideal para un look fresco que se mantiene todo el día.",
             },
             {
                 id: 6,
-                productimage: "https://production.na01.natura.com/on/demandware.static/-/Sites-natura-ar-storefront-catalog/default/dw3ef1caeb/NATARG-95757_1.jpg",
+                productimage: "/image/delineador.jpg",
                 productname: "Delineador líquido waterproof",
                 productdescription: "Punta ultra precisa para trazos definidos o dramáticos. Secado rápido y a prueba de todo.",
             },
             {
                 id: 7,
-                productimage: "https://cdn2.primor.eu/media/catalog/product/0/M/0MO20544_1_c58b.jpg",
+                productimage: "/image/mascara.jpg",
                 productname: "Máscara de pestañas volumen extremo",
                 productdescription: "Alarga, curva y da volumen desde la primera pasada. No se corre ni se descascara.",
             },
             {
                 id: 8,
-                productimage: "https://m.media-amazon.com/images/I/71Z6khL9EPL._SL1500_.jpg",
+                productimage: "/image/cejas.jpg",
                 productname: "Lápiz de cejas retráctil",
                 productdescription: "Trazo fino y natural para definir, rellenar y dar forma con precisión. Ideal para lograr unas cejas prolijas y con acabado impecable.",
             },
             {
                 id: 9,
-                productimage: "https://http2.mlstatic.com/D_NQ_NP_833586-MLA79890068758_102024-O.webp",
+                productimage: "/image/iluminador.jpg",
                 productname: "Iluminador en polvo",
                 productdescription: "Brillo radiante sin partículas gruesas. Ideal para darle luz a mejillas, nariz y arco de cupido.",
             },
             {
                 id: 10,
-                productimage: "https://getthelookar.vtexassets.com/arquivos/ids/164106/222118_labial-liquido-maybelline-superstay-matte-ink-x-5-ml_imagen-1.jpg?v=637612688756770000",
+                productimage: "/image/labial.jpg",
                 productname: "Labial líquido mate",
                 productdescription: "Color intenso que no se transfiere. Textura ligera, no reseca y se mantiene intacto por horas.",
             }
@@ -89,3 +89,5 @@ const index = {
         ]
     }
 }
+
+module.exports = index
